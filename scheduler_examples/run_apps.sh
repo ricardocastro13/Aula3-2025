c7ccf643f685954d67252e32fa25d331f2a4a2a3
ricardocastro13 <a22402222@alunos.ulht.pt>
2025-10-01 10:52:09 +0100

    sss

--- scheduler_examples/run_apps.sh
+++ scheduler_examples/run_apps.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
-# This script runs multiple applications in the background.
-# It waits for all of them to finish before exiting.
+
 ./app outlook 10 &
 ./app firefox 15 &
 ./app powerpoint 20 &
